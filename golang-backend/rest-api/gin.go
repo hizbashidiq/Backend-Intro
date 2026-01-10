9a9e329a7e0252809895490f8a0ded407920f990
hizbashidiq <hizbulloh.ash@gmail.com>
2026-01-10 13:49:32 +0700

    rest api golang
    context, logging, input validation (mostly in gin using validator/v10)

--- golang-backend/rest-api/gin.go
+++ golang-backend/rest-api/gin.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/pelletier/go-toml/v2"
 )
 
 var sGin = gin.New()
@@ -33,9 +38,111 @@ func runGin(){
     c.JSON(http.StatusOK, gin.H{"status":"you are logged in"})
   })
 
+  sGin.POST("/test", handler)
+
+  sGin.GET("/test/:details", handler)
+
+  sGin.POST("/price", func(c *gin.Context) {
+    body := Body{}
+    if err:=c.ShouldBindJSON(&body); err!=nil{
+      var ve validator.ValidationErrors
+      if errors.As(err, &ve){
+        out := make([]ErrMsg, len(ve))
+        for i, fe:= range ve{
+          out[i] = ErrMsg{fe.Field(), getErrMsg(fe)}
+        }
+        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"errors": out})
+      }
+      // c.AbortWithStatusJSON(http.StatusBadRequest,
+      //   gin.H{
+      //     "error": "VALIDATEERR-1",
+      //     // "message": "invalid inputs. Please check your inputs",
+      //     "message":err.Error(),
+      //   })
+      return
+    }
+    c.JSON(http.StatusOK, &body)
+
+  })
+
+  sGin.POST("/toml", func(c *gin.Context){
+    uri := URI{}
+    // shouldbind is i/o stream while shouldbindbody is memory buffer
+    // mostly you will only need shouldbind unless advance and specific case of middleware
+    if err:=c.ShouldBindWith(&uri, Toml{});err!=nil{
+      c.AbortWithError(http.StatusBadRequest, err)
+      return
+    }
+    c.JSON(http.StatusOK, uri)
+  })
+
   log.Fatal(http.ListenAndServe(":8080", sGin))
 }
 
-func HelloHandlerGin(){
+func handler(c *gin.Context){
+  details := c.Param("details")
 
+  if details == ""{
+    body := Body{}
+    if err:=c.BindJSON(&body);err!=nil{
+      // bindjson is stream I/O, while shouldbindbodywith copy the body buffer and add it to context
+      c.AbortWithError(http.StatusBadRequest, err)
+      return
+    }
+    fmt.Println(body)
+    c.JSON(http.StatusAccepted, &body)
+  }else{
+    uri := URI{}
+    if err:=c.BindUri(&uri);err!=nil{
+      c.AbortWithError(http.StatusBadRequest, err)
+      return
+    }
+    fmt.Println(uri)
+    c.JSON(http.StatusAccepted, &uri)
+  }
+}
+
+type ErrMsg struct{
+  Field string `json:"field"`
+  Message string `jsong:"message"`
+}
+
+func getErrMsg(fe validator.FieldError) string{
+  switch fe.Tag(){
+  case "required":
+    return "This field is required"
+  case "lte":
+    return "Should be less than "+fe.Param()
+  case "gte":
+    return "Should be more than "+fe.Param()
+  }
+  return "Unknown Error"
+}
+
+type Toml struct{
+}
+
+// customize binding need Name() and Bind(r *http.Request, i interface{}) method
+func (t Toml)Name() string{
+  return "toml"
+}
+
+func (t Toml)Bind(r *http.Request, i interface{}) error{
+  tD := toml.NewDecoder(r.Body)
+  return tD.Decode(i)
+}
+
+// to be able to use ShouldBindBodyWith, need to implement BindBody method
+func (t Toml)BindBody(bytes []byte, i interface{})error{
+  return toml.Unmarshal(bytes, i)
+}
+
+type URI struct{
+  Details string `json:"name" uri:"details" binding:"required"`
+  // uri:details is a gin tag. it tells to bind path parameter named :details
+}
+
+type Body struct{
+  Name string `json:"name"`
+  Price uint `json:"price" binding:"required,gte=10,lte=1000"`
 }
--- golang-backend/rest-api/main.go
+++ golang-backend/rest-api/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+
 	// "strings"
 
 	// "net/http"
 	"os"
 	// "log"
 	// "github.com/gin-gonic/gin"
+	"context"
+	"time"
 )
 
 func main(){
@@ -79,7 +83,7 @@ func main(){
   // Gin -> big app, REST API that need high level performance and complete feature
   framework := ""
   // framework = "default"
-  // framework = "gin"
+  framework = "gin"
   if framework == "default"{
     runDefault()
   }
@@ -94,6 +98,151 @@ func main(){
   fmt.Printf("environment = %s \n", os.Getenv("APP_ENV"))
 
   // mostly you'll only need to read ENV, you'll not need to manage it's lifecycle
+
+  // context -> easier to manage passing request-scoped value, cancellation signal, and deadline to all goroutine
+  // WithValue(), WithCancel(), WithDeadline(), WithTimeout()
+  // context interface method: Deadline(), Done(), Err(), Value()
+  // best practices -> pass context as the first argument, always call CancelFunc to release the resource even
+  // if it's succeed and not canceled (often using defer cancel())
+
+  // background is the root of any context tree and never canceled
+
+  // ctx := context.TODO()
+  ctx := context.Background()
+
+  ctx = context.WithValue(ctx, "myKey", "MyVal")
+
+  // context.todo() and context.background() basically the same, it's just convey different intent of dev
+  doSomething(ctx)
+
+  // value stored in specific context is immutable
+
+  // best practice-if you need a data to run a function, pass it as an argument instead store it in context
+
+  // if more than one case fulfill the condition in select statement, it will be chosen randomly
+
+  ctx = context.Background()
+  doSomething1(ctx)
+
+  // errors.New and fmt.Errorf basically the same but usually .New will be used
+
+
+  for i:=1;i<4;i++{
+    fmt.Printf("validating...%d", i)
+    // if err:=validate(i);err == itsTwo{
+    //   fmt.Println("this is error two")
+    // }else if err!=nil{
+    //   fmt.Println("there's an error")
+    // }else{
+    //   fmt.Println("valid!")
+    // }
+    err := runValidate(i)
+
+    var valueErr *ValueError
+    // if err == itsTwo || errors.Unwrap(err) == itsTwo || errors.Unwrap(errors.Unwrap(err)) == itsTwo{
+    if errors.Is(err, itsTwo){
+      // errors.Is using error type unwrap method which mean you still need to implement unwrap method in your custom
+      // error struct
+      fmt.Println("Oh no!")
+    }else if errors.As(err, &valueErr){
+      fmt.Printf("value error (%d): %v\n", valueErr.Value, valueErr.Err)
+    }else if err!=nil{
+      fmt.Println("there was an error :", err)
+    }else{
+      fmt.Println("valid!")
+    }
+  }
+
+  // input validation should happen as early as possible in the data flow, preferably as soon as the data received
+  // from the external party
+
+  // syntatic validation should enforce correct syntax of structured fields (e.g. date, currency symbol)
+  // semantic validation should enforce correct value regarding business context (e.g. start date before end date)
+}
+
+
+// custom error type usually has a suffix of Error
+type ValueError struct{
+  Value int
+  Err error
+}
+
+func newValueError(value int, err error) *ValueError{
+  return &ValueError{
+    Value : value,
+    Err : err,
+  }
+}
+func (ve *ValueError) Error() string{
+  return fmt.Sprintf("value error: %s", ve.Err)
+}
+func (ve *ValueError) Unwrap() error{
+  return ve.Err
+}
+
+var itsTwo = errors.New("it's 2")
+
+func runValidate(i int) error{
+  if err := validate(i); err!=nil{
+    return fmt.Errorf("run error: %w", err)
+  }
+  return nil
+}
+
+func validate(i int) error{
+  if i == 1{
+    // return fmt.Errorf("it's 1")
+    return newValueError(i, fmt.Errorf("it's 1"))
+  }else if i == 2{
+    // return itsTwo
+    return newValueError(i, itsTwo)
+  }else{
+    return nil
+  }
+}
+
+func doSomething1(ctx context.Context){
+  ctx, cancelCtx := context.WithCancel(ctx)
+
+  printCh := make(chan int)
+
+  go doAnother1(ctx, printCh)
+  for i:=0;i<=3;i++{
+    printCh<-i
+  }
+  cancelCtx()
+  time.Sleep(100*time.Millisecond)
+  fmt.Printf("doSomething1: finished\n")
+
+}
+
+func doAnother1(ctx context.Context, ch <-chan int){ //it's kinda a worker
+  for{
+    select{
+    case <-ctx.Done():
+      if err:=ctx.Err(); err!=nil{
+        fmt.Printf("Do Another1: Error:%s\n", err)
+      }
+      fmt.Printf("Do Another1: Finished\n")
+      return
+    case num:=<-ch:
+      fmt.Printf("doAnother1: %d\n", num)
+    }
+  }
+}
+
+func doSomething(ctx context.Context){
+  fmt.Printf("Do Something: myKey's value is %s\n", ctx.Value("myKey"))
+
+  anotherCtx := context.WithValue(ctx, "myKey", "anotherVal")
+  doAnother(anotherCtx)
+  // when using ctx.value, it will find outer most wrapped value for the given key
+
+  fmt.Printf("Do Something: myKey's value is %s\n", ctx.Value("myKey"))
+}
+
+func doAnother(ctx context.Context){
+  fmt.Printf("Do Another: myKey's value is %s\n", ctx.Value("myKey"))
 }
 
 func envVariable(key string) string{
